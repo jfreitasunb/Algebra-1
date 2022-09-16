5e20e9f3a221326221b930a5c2d1712d23c727d5
José Antônio Oliveira de Freitas <jfreitas@unb.br>
2022-09-15 22:02:58 -0300

    Ajustes estéticos e mudanças nos nomes de variáveis.

--- animacoes/simetria_triangulo_equilatero.py
+++ animacoes/simetria_triangulo_equilatero.py
@@ -5,22 +5,22 @@ def finishScene(self):
 
 class Start(Scene):
     def construct(self):
-        rref_matrix_p1 = Matrix(
+        f = Matrix(
             [("1", "2", "3"), ("3", "1", "2")], left_bracket="(",
             right_bracket=")")
 
-        rref_matrix_p1_flat = VGroup(*VGroup(*rref_matrix_p1)[0])
-        texto = Tex(r"f=")
+        f_flat = VGroup(*VGroup(*f)[0])
+        texto = Tex(r"f\ =\ ")
 
-        rref_matrix_p1.to_corner(corner=UP + LEFT*2, buff=0.5)
+        f.to_corner(corner=UP + LEFT*3, buff=0.5)
 
-        texto.next_to(rref_matrix_p1, LEFT)
+        texto.next_to(f, LEFT)
         self.play(Write(texto))
-        for i in VGroup(*rref_matrix_p1)[1:]:
+        for i in VGroup(*f)[1:]:
             self.play(Write(i))
 
         for i in range(0,6):
-            self.play(Write(rref_matrix_p1_flat[i]), **{"run_time": 0.75})
+            self.play(Write(f_flat[i]), **{"run_time": 0.75})
         self.wait(4)
         triangulo = Triangle().scale(2)
 
@@ -49,23 +49,50 @@ class Start(Scene):
         labels_originais.set_color(RED)
 
         Triangulo_rotacao = VGroup()
-        Triangulo_rotacao.add(triangulo, labels_rotacionados)
+        Triangulo_rotacao.add(triangulo.copy(), labels_rotacionados.copy())
 
         self.play(Write(labels_originais))
-        self.play(FadeIn(triangulo))
+        self.play(FadeIn(Triangulo_rotacao[0]))
 
         # ... some move or rotate mobjects around...
         self.play(Rotate(Triangulo_rotacao, 2*PI/3, about_point = baricentro, rate_func = linear, run_time = 3))
 
         self.play(Rotate(Triangulo_rotacao[1][0], -2*PI/3), Rotate(Triangulo_rotacao[1][1], -2*PI/3), Rotate(Triangulo_rotacao[1][2], -2*PI/3))
 
-        Triangulo_reflexao = VGroup()
-        Triangulo_reflexao.add(triangulo, labels_rotacionados)
+        self.play(
+            *[FadeOut(mob)for mob in self.mobjects]
+        )
+        g = Matrix(
+            [("1", "2", "3"), ("1", "3", "2")], left_bracket="(",
+            right_bracket=")")
 
-        self.wait(4)
+        g_flat = VGroup(*VGroup(*g)[0])
+
+        texto_g = Tex(r"g\ =\ ")
+
+        g.to_corner(corner=UP + LEFT*3, buff=0.5)
+
+        texto_g.next_to(g, LEFT)
+        self.play(Write(texto_g))
+        for i in VGroup(*g)[1:]:
+            self.play(Write(i))
+
+        for i in range(0,6):
+            self.play(Write(g_flat[i]), **{"run_time": 0.75})
+
+        self.play(Write(labels_originais))
+
+        self.play(FadeIn(triangulo))
+
+        Triangulo_reflexao = VGroup()
+
+        Triangulo_reflexao.add(triangulo, labels_rotacionados)
         matrix = [[-1, 0], [0, 1]]
+
         self.play(Triangulo_reflexao.animate.apply_matrix(matrix), run_time=1)
-        #self.add(Triangulo_reflexao[1][0].apply_matrix(matrix), Triangulo_reflexao[1][0].move_to(vertices_rotacionados[2]), Triangulo_reflexao[1][1].apply_matrix(matrix), Triangulo_reflexao[1][1].move_to(vertices_rotacionados[1]), Triangulo_reflexao[1][2].apply_matrix(matrix), Triangulo_reflexao[1][2].move_to(vertices_rotacionados[0]))
+
+        self.add(Triangulo_reflexao[1][0].apply_matrix(matrix), Triangulo_reflexao[1][0].move_to(vertices_rotacionados[0]), Triangulo_reflexao[1][1].apply_matrix(matrix), Triangulo_reflexao[1][1].move_to(vertices_rotacionados[2]), Triangulo_reflexao[1][2].apply_matrix(matrix), Triangulo_reflexao[1][2].move_to(vertices_rotacionados[1]))
+
         self.wait(4)
 
         finishScene(self)
